0b09a15ff4c1c547befa879bec1e2afc70661acd
Madhur Sharma <madhursharma@Mac.lan>
2025-11-01 15:11:16 +0530

    add npx shadcn@latest init

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div>
-      <h2>Home</h2>
+    <div className='p-10 bg-shop_light_pink'>
+      <h2 className='text-xl font-semibold'>Home</h2>
       <p> 
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur quas, ea est quia voluptatem velit inventore repellat possimus eius, laborum in ex illo assumenda ad consequuntur hic iusto! Suscipit, dicta quas, natus maiores, sequi esse perspiciatis veniam numquam ad dignissimos velit sit? Ratione dolorem voluptatibus porro maxime, odio facilis modi.
       </p>
