f7e99f18461dee688a51af13caf05ef4845cb093
Madhur Sharma <madhursharma@Mac.lan>
2025-11-01 16:15:48 +0530

    Updated UI components and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from '../components/ui/button'
 
 const HomePage = () => {
   return (
@@ -7,6 +8,7 @@ const HomePage = () => {
       <p> 
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur quas, ea est quia voluptatem velit inventore repellat possimus eius, laborum in ex illo assumenda ad consequuntur hic iusto! Suscipit, dicta quas, natus maiores, sequi esse perspiciatis veniam numquam ad dignissimos velit sit? Ratione dolorem voluptatibus porro maxime, odio facilis modi.
       </p>
+  <Button size="lg">Check out</Button>
     </div>
 
   );
