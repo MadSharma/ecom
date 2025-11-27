b03482bfc091c164548e72eee2eda0e80a2fa12f
Madhur Sharma <madhursharma@Mac.lan>
2025-11-27 10:49:50 +0530

    add hover effect

--- components/ui/HeaderMenu.tsx
+++ components/ui/HeaderMenu.tsx
@@ -4,14 +4,31 @@ import React from 'react'
 
 const HeaderMenu = () => {
   return (
-    <div className='hidden md:inline-flex w-1/3'>
+    <div className="hidden md:inline-flex w-1/3 items-center gap-7 text-sm capitalize font-semibold text-lightColor">
       {headerData?.map((item) => (
-          <Link key={item?.title} href={item?.href}>
-            {item?.title}
-          </Link> 
-     ))}   
+        <Link
+          key={item?.title}
+          href={item?.href}
+          className="relative group hover:text-shop_light_green hoverEffect"
+        >
+          {item?.title}
+
+          {/* Left underline bar */}
+          <span
+            className="absolute -bottom-1 left-1/2 w-0 h-0.5 bg-shop_light_green 
+            transition-all duration-300 group-hover:w-1/2 group-hover:left-0"
+          ></span>
+
+          {/* Right underline bar */}
+          <span
+            className="absolute -bottom-1 right-1/2 w-0 h-0.5 bg-shop_light_green 
+            transition-all duration-300 group-hover:w-1/2"
+          ></span>
+
+        </Link>
+      ))}   
     </div>
   )
 }
 
-export default HeaderMenu
+export default HeaderMenu;
