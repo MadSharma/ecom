145a3fb2d14d7afc5171acc75201beb8bd270182
Madhur Sharma <madhursharma@Mac.lan>
2025-11-27 23:37:36 +0530

    add side bar menu opeions

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -1,23 +1,40 @@
-import React ,{FC} from 'react'
+import React ,{FC, use} from 'react'
 import Logo from './Logo';
 import { X } from 'lucide-react';
+import Link from 'next/link';
+import { headerData } from '@/constants/data';
+import { usePathname } from 'next/navigation';
+
 interface SideMenuProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
 const SideMenu: FC<SideMenuProps> = ({ isOpen, onClose }) => {
+  const pathname = usePathname();
   return (
-    <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/80 shadow-xl 
+    <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/70 shadow-xl 
       ${isOpen ? "translate-x-0" :  "-translate-x-full"} hoverEffect`}>
-      <div>
+     
         <div className='min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop-_dark_green flex flex-col gap-6 '>
-          <Logo className='text-white' />
+        <div className='flex items-center justify-between gap-5'>
+          <Logo className='text-white' spanDesign="group-hover:text-white" />
           <button onClick={onClose} className='hover:text-shop_light_green hoverEffect' >
             <X />
           </button>
         </div>
-      </div>
+        <div className='flex flex-col space-y-3.5 font-semibold tracking-wide'>
+          {headerData?.map((item) => (
+            <Link 
+              href={item?.href ?? '#'}
+              key={item?.title}
+              className={`hover:text-shop_light_green ${pathname ===item?.href && "text-white"} hoverEffect`}
+            >
+              {item?.title}
+            </Link>
+          ))}
+        </div>
+       </div>
     </div>
   )
 }
